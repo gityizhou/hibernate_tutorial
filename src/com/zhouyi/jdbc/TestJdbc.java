1af46c6d85099de398c8797ac5e823209343a704
gityizhou <zxc47POI>
2019-07-04 16:00:23 +0800

    hibernate initialization, create tables, entity, add basic annotation

---
+++ src/com/zhouyi/jdbc/TestJdbc.java
@@ -0,0 +1,23 @@
+package com.zhouyi.jdbc;
+import java.sql.*;
+
+public class TestJdbc {
+
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false" +
+                "&serverTimezone=UTC";
+        String user = "root";
+        String password = "zxc47POI!";
+
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Connecting to db " + url);
+            Connection conn = DriverManager.getConnection(url,user,password);
+            System.out.println("connected to db");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
